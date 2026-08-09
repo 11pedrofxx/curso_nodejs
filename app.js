8cb352b7e3a0c25991bbd7fa5948fd3da417d3f0
Pedro <ph730679@gmail.com>
2026-08-09 12:37:41 -0300

    A

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('\nOlá Mundo\n')
+import express from 'express'; // express é a base da construção da API
+const servidor = express(); // variavel criada com const invés de let pq const nn pode ser alterado pois o servidor não pra que ficar mudando.
+
+
+servidor.get('/helloworld', (req, resp) => {
+    // codigo do end point
+    resp.send('Hello world!');
+
+})
+
+
+servidor.listen(
+    5001,
+     () => console.log('API subiu com sucesso na porta 5001!'))
